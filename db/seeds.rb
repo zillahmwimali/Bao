d7938d6ecc4c7ca912302bbea77c80dd0c5dc0c8
Zillah Mwimali <zillahmwimmali@gmail.com>
2025-08-24 00:38:30 +0300

    fix: move seed variables to env file & declare seed environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-User.find_or_create_by!(email: "zeemwimali@gmail.com") do |user|
-  user.name = "Zillah "
-  user.phone = "0712345678"
+if Rails.env.development? || Rails.env.test? || ENV["SEED_ADMIN"] == "true"
+User.find_or_create_by!(email: ENV.fetch("ADMIN_EMAIL")) do |user|
+  user.name = ENV.fetch("ADMIN_NAME", "Bao Admin")
+  user.phone = ENV["ADMIN_PHONE"]
   user.user_role = "admin"
-  user.password = "Bao2025!"
-  user.password_confirmation = "Bao2025!"
+  user.password = ENV.fetch("ADMIN_PASSWORD")
+  user.password_confirmation = ENV.fetch("ADMIN_PASSWORD")
+end
 end
